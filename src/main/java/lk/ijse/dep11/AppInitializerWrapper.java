2e06b7aa4b676cdf44493553b1b29a15c3053a2d
Janaka Daskon <janakadaskon@gmail.com>
2023-09-19 16:31:42 +0530

    Initiate the project

---
+++ src/main/java/lk/ijse/dep11/AppInitializerWrapper.java
@@ -0,0 +1,16 @@
+package lk.ijse.dep11;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializerWrapper extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
