d7aa589046ec9f719a8b9fc1713ed3600a921644
Janaka Daskon <janakadaskon@gmail.com>
2023-09-19 19:02:02 +0530

    Design Finished

--- src/main/java/lk/ijse/dep11/MainFormController.java
+++ src/main/java/lk/ijse/dep11/MainFormController.java
@@ -1,4 +1,18 @@
 package lk.ijse.dep11;
 
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class MainFormController {
+    public AnchorPane root;
+    public Slider slrRED;
+    public TextField txtRED;
+    public Slider slrGREEN;
+    public TextField txtGREEN;
+    public Slider slrBLUE;
+    public TextField txtBLUE;
+    public Slider slrALPHA;
+    public TextField txtALPHA;
+    public TextField txtCOLOR;
 }
