7b782678322e98be740209dfebf6aeb9333c6d96
Janaka Daskon <janakadaskon@gmail.com>
2023-09-19 19:17:30 +0530

    Inialize method

--- src/main/java/lk/ijse/dep11/MainFormController.java
+++ src/main/java/lk/ijse/dep11/MainFormController.java
@@ -2,17 +2,33 @@ package lk.ijse.dep11;
 
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
 
 public class MainFormController {
-    public AnchorPane root;
-    public Slider slrRED;
-    public TextField txtRED;
-    public Slider slrGREEN;
-    public TextField txtGREEN;
-    public Slider slrBLUE;
-    public TextField txtBLUE;
-    public Slider slrALPHA;
-    public TextField txtALPHA;
-    public TextField txtCOLOR;
+
+
+    public Slider slrRed;
+    public TextField txtRed;
+    public Slider slrGreen;
+    public TextField txtGreen;
+    public Slider slrBlue;
+    public TextField txtBlue;
+    public Slider slrAlpha;
+    public TextField txtAlpha;
+    public Rectangle shpColor;
+    public TextField txtColor;
+
+    public void initialize(){
+        txtRed.setTooltip(new Tooltip("RED Value"));
+        txtGreen.setTooltip(new Tooltip("GREEN Value"));
+        txtBlue.setTooltip(new Tooltip("BLUE Value"));
+        txtAlpha.setTooltip(new Tooltip("ALPHA value"));
+        txtColor.setTooltip(new Tooltip("COLOR Value"));
+
+        slrRed.valueProperty().addListener(e -> {
+            txtRed.setText(String.format("%.0f",slrRed.getValue()));
+        });
+    }
 }
